833b923b358246be936ed896ac48266c7da6f0d3
akamhieh <akamhieh@dr-coskun.com>
2022-10-09 11:58:06 +0200

    Add project files.

---
+++ IEnumerator_VS_IEnumerable/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3); 
+        list.Add(4);
+
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine(list[i]);
+        }
+        IterateNext(list);
+
+        Console.WriteLine("---------------------------");
+
+        var enumerator = list.GetEnumerator();
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+        IterateNext(enumerator);
+
+    }
+
+    public static void IterateNext(IEnumerator<int> enumerator)
+    {
+        while (enumerator.MoveNext())
+        {
+            Console.WriteLine(enumerator.Current);
+        }
+    }
+
+    public static void IterateNext(IEnumerable<int> enumerable)
+    {
+        foreach (var item in enumerable)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
